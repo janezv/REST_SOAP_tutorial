3d58a201a2898f43a1ed89f513d2dd8488d63f7f
Janez Vegan <janez.vegan@gmail.com>
2024-09-17 22:23:05 +0200

    Reac App

--- APP/React/api-random/src/App.js
+++ APP/React/api-random/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch('https://random-data-api.com/api/v2/users')
+      .then(response => response.json())
+      .then(json => setData(json))
+      .catch(error => console.error(error));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1> Ahoj</h1>
+      <h2>{data.first_name}</h2>
+      <h2>{data.last_name}</h2>
+      {data ? <pre>{JSON.stringify(data, null, 2)}</pre> : 'Loading...'}
     </div>
   );
 }
