47af6ad57fdf72ce6eecaf0c68bf3979ecb4dbc4
Janez Vegan <janez.vegan@gmail.com>
2024-09-22 13:57:17 +0200

    koncano

---
+++ APP/C# App/DotNetSOAPStarter/Controllers/WeatherForecastController copy.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetSOAPStarter.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ServicelController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[] {
+            "Freezing","Bracing","Chilly","Cool","Mild","Warm","Balmy","Hot","Sweltering","Scorching"
+        };
+
+        private readonly ILogger<ServicelController> _logger;
+
+        public ServicelController(ILogger<ServicelController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        [Produces("application/xml")]
+        public IEnumerable<WeatherForecast> Get() { 
+            return Enumerable.Range(1,5).Select(index => new WeatherForecast 
+            { 
+                Date=DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20,55),
+                Summary=Summaries[Random.Shared.Next(Summaries.Length)]
+            
+            })
+                .ToArray(); 
+        
+        }
+
+    }
+}
---
+++ APP/C# App/tmp/ConsoleApp1/Program.cs
@@ -0,0 +1,51 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Xml.Linq;
+
+public class WeatherForecast
+{
+    public System.DateTime Date { get; set; } = DateTime.Now;
+    public int TempC { get; set; } = 16;
+    public int TempF => 32 + (int)(TempC / 0.5556);
+    public string Summary { get; set; } = "Poletje";
+
+};
+
+public class d2Index
+{
+    public int index { get; set; }
+    public int vrednost { get; set; }
+}
+
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        //preprost IEnumerable <int> pirmer
+        Random random = new Random(); // Ustvari nov primer razreda Random
+        IEnumerable<d2Index> listInt = Enumerable.Range(1,8).Select(index =>new d2Index { index= index, vrednost=random.Next(0,10)});
+        foreach (var element in listInt)
+        {
+            Console.WriteLine(element.index + "  "+element.vrednost);
+        }
+
+
+        // Primer iz WeatherForecast 
+
+        string[] Summaries = new[] {
+            "Freezing","Bracing","Chilly","Cool","Mild","Warm","Balmy","Hot","Sweltering","Scorching"
+        };
+
+        IEnumerable<WeatherForecast> listWeather= Enumerable.Range(1, 8).Select(index=>new WeatherForecast { 
+            Date = DateTime.Now,
+            TempC=16,
+            Summary= Summaries[Random.Shared.Next(Summaries.Length)]
+        });
+        foreach (var element in listWeather)
+        {
+            Console.WriteLine(element.Date + "  " + element.TempC+" " +element.TempF + "; "+ element.Summary);
+        }
+
+    } 
+}
